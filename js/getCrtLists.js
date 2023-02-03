5781956bd7e18f100f6bc149dbd5016c1f260054
tladbwls <zipte@naver.com>
2023-02-03 11:03:05 +0900

    0203

--- js/getCrtLists.js
+++ js/getCrtLists.js
@@ -2,15 +2,13 @@ const keyParam = document.location.href.split("=")[1];
 const decordParam = decodeURI(keyParam);
 
 function getCrtLists(latitude, longitude) {
-  console.log(latitude);
-  console.log(longitude);
   $.ajax({
     url: `/EV_CHARGE/php/search_list.php?key=${decordParam}`, //요청 엔드포인트
     type: "GET", // 요청 방식
     success: function (data) {
-      // console.log(data);
       const parseJson = JSON.parse(data); //  json 문자열을 json 데이터 형식으로 파싱
       const items = parseJson.body.items.item;
+      console.log(items);
       console.log(items);
       console.log(items[0].lat);
       console.log(items[0].longi);
@@ -28,6 +26,7 @@ function getCrtLists(latitude, longitude) {
       var imageSrc = [];
       for (let i = 0; i < items.length; i++) {
         positions.push({
+          content: `<div class="marker-info">${items[i].csNm}</div>`,
           latlng: new kakao.maps.LatLng(items[i].lat, items[i].longi),
         });
 
@@ -37,8 +36,6 @@ function getCrtLists(latitude, longitude) {
           imageSrc.push("/ev_charge/img/red.png");
         }
       }
-
-      console.log(imageSrc);
       // 마커 이미지의 이미지 주소입니다
       // var imageSrc = "/EV_CHARGE/img/green.png";
 
@@ -56,10 +53,100 @@ function getCrtLists(latitude, longitude) {
           title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
           image: markerImage, // 마커 이미지
         });
+
+        // 마커에 표시할 인포윈도우를 생성합니다
+        var infowindow = new kakao.maps.InfoWindow({
+          content: positions[i].content, // 인포윈도우에 표시할 내용
+        });
+        //각 마커의 제목을 말풍선으로 표시합니다.
+        infowindow.open(map, marker);
       }
+
+      //말풍선 정보 스타일 수정
+      const markerInfo = document.querySelectorAll(".marker-info");
+      markerInfo.forEach((info) => {
+        info.parentNode.parentNode.style.background = "rgb(34,36,41";
+        info.parentNode.style.width = 100 + "%";
+        info.parentNode.style.height = 100 + "%";
+        info.parentNode.style.border = "#fff";
+      });
+
+      getLists(latitude, longitude, data);
     },
     error: function (e) {
       console.log(e);
     },
+  });
+}
+
+function getLists(la, ln, d) {
+  const parseJson = JSON.parse(d); //  json 문자열을 json 데이터 형식으로 파싱
+  const items = parseJson.body.items.item;
+  const itemList = document.querySelector(".item-list");
+  let listsElmt;
+
+  items.map((li) => {
+    let charTp, cpStat, statBg;
+    if (li.charTp === "1") {
+      charTp = "완속";
+    } else {
+      charTp = "급속";
+    }
+    if (li.cpStat === "1") {
+      cpStat = "충전가능";
+      statBg = "rgb(50,151,255)";
+    } else if (li.cpStat === "2") {
+      cpStat = "충전중";
+      statBg = "rgb(210,145,188)";
+    } else {
+      cpStat = "오류(점검중)";
+      statBg = "rgb(255,50,50)";
+    }
+
+    function getDistanceFromLatLonInKm(lat1, lng1, lat2, lng2) {
+      function deg2rad(deg) {
+        return deg * (Math.PI / 180);
+      }
+
+      var R = 6371; // Radius of the earth in km
+      var dLat = deg2rad(lat2 - lat1); // deg2rad below
+      var dLon = deg2rad(lng2 - lng1);
+      var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) *
+          Math.cos(deg2rad(lat2)) *
+          Math.sin(dLon / 2) *
+          Math.sin(dLon / 2);
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      var d = R * c; // Distance in km
+      return d;
+    }
+
+    console.log(
+      getDistanceFromLatLonInKm(
+        37.555391,
+        126.919973,
+        37.56838316139128,
+        126.89494282897327
+      )
+    );
+
+    listsElmt = `
+        <div class="item">
+        <p>${li.addr}</p>
+        <div class="stats">
+          <span style = "background:${statBg}">상태 : ${cpStat}</span>
+          <span>타입 : ${charTp}</span>
+          <span>갱신 : ${li.statUpdateDatetime}</span>
+          <span>거리 : ${getDistanceFromLatLonInKm(
+            la,
+            ln,
+            li.lat,
+            li.longi
+          ).toFixed(2)}km</span>
+        </div>
+      </div>
+    `;
+    itemList.innerHTML += listsElmt;
   });
 }
